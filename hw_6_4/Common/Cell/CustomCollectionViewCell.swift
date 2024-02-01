2d37d44652a4f3fe65847e92001866332a16b4bd
ErmaIOS <tanikulovermekios@gmail.com>
2024-02-01 19:05:50 +0600

    .

---
+++ hw_6_4/Common/Cell/CustomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCollectionViewCell.swift
+//  hw_6_4
+//
+//  Created by Erma on 1/2/24.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ hw_6_4/MakeView/MakeView.swift
@@ -0,0 +1,8 @@
+//
+//  MakeView.swift
+//  hw_6_4
+//
+//  Created by Erma on 1/2/24.
+//
+
+import Foundation
