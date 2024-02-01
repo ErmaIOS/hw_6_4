0cffcb4cb8ac8b9354ad628ed144a846e36299bd
ErmaIOS <tanikulovermekios@gmail.com>
2024-02-01 19:05:57 +0600

    .

--- hw_6_4/Common/Cell/CustomCollectionViewCell.swift
+++ hw_6_4/Common/Cell/CustomCollectionViewCell.swift
@@ -9,4 +9,40 @@ import UIKit
 
 class CustomCollectionViewCell: UICollectionViewCell {
     
+    
+    private lazy var imageSalad:UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFill
+        view.layer.cornerRadius = 16
+        view.layer.masksToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    
+    
+    private func setupUI(){
+        contentView.addSubview(imageSalad)
+        imageSalad.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.horizontalEdges.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+        
+    }
+    
+    
+    func setImage(_ image: String){
+        imageSalad.image = UIImage(named: image)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
--- hw_6_4/MakeView/MakeView.swift
+++ hw_6_4/MakeView/MakeView.swift
@@ -5,4 +5,59 @@
 //  Created by Erma on 1/2/24.
 //
 
-import Foundation
+import UIKit
+
+class MakeView{
+    
+    static let shared = MakeView()
+    
+    func makerLabel(text:String = "",
+                    textColor: UIColor = .white,
+                    textSize:CGFloat = 20,
+                    textWeight:UIFont.Weight = .regular,
+                    textNumberOfLines: Int = 0
+                    ) -> UILabel {
+        let view = UILabel()
+        view.text = text
+        view.textColor = textColor
+        view.font = .systemFont(ofSize: textSize,weight: textWeight)
+        view.numberOfLines = textNumberOfLines
+        return view
+    }
+    
+
+    
+    func makerImage(imageName: String = "",
+                    imageContentMode: UIView.ContentMode = .scaleAspectFill,
+                    imageBackgroundColor: UIColor = .white,
+                    imageTintColor: UIColor = .white
+                    )-> UIImageView{
+        let view = UIImageView()
+        view.image = UIImage(named: imageName)
+        view.contentMode = imageContentMode
+        view.backgroundColor = imageBackgroundColor
+        view.tintColor = imageTintColor
+        return view
+    }
+    
+    func  makerCollectionView(cvItemWidth: CGFloat,
+                              cvItemSize: CGFloat,
+                              cvMinimumLineSpacing: CGFloat = 20,
+                              cvMinimumInteritemSpacing: CGFloat = 0,
+                              cvScrollDirection: UICollectionView.ScrollDirection = .vertical,
+                              cvCornerRadius: CGFloat = 16,
+                              cvBackgroundColor: UIColor?
+    ) -> UICollectionView{
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = .init(width: cvItemWidth, height: cvItemSize)
+        layout.minimumLineSpacing = cvMinimumLineSpacing
+        layout.minimumInteritemSpacing = cvMinimumInteritemSpacing
+        layout.scrollDirection = cvScrollDirection
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.layer.cornerRadius = cvCornerRadius
+        cv.backgroundColor = cvBackgroundColor
+        return cv
+    }
+    
+}
+
